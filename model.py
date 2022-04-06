8e3a6adc0dc48d1148d9bdea09554d315fe424f2
tianliDing <dtl6303@gmail.com>
2022-04-05 22:29:47 -0500

    clean

--- model.py
+++ model.py
@@ -48,7 +48,7 @@ def unigram(train_set, train_labels, dev_set, smoothing_parameter=0.3, pos_prior
                 neg_total += punk_neg
 
         pos_total += math.log(pos_prior)
-        neg_total += math.log(1-math.log(pos_prior))
+        neg_total += math.log(1 - math.log(pos_prior))
 
         if pos_total >= neg_total:
             res.append(1)
@@ -58,7 +58,7 @@ def unigram(train_set, train_labels, dev_set, smoothing_parameter=0.3, pos_prior
 
 
 def bigram(train_set, train_labels, dev_set, unigram_smoothing_parameter=0.035, bigram_smoothing_parameter=0.02,
-                bigram_lambda=0.08, pos_prior=0.8):
+           bigram_lambda=0.08, pos_prior=0.8):
     """
     train_set -  [['like','this','movie'], ['i','fall','asleep']]
 
@@ -78,15 +78,9 @@ def bigram(train_set, train_labels, dev_set, unigram_smoothing_parameter=0.035, 
 
         for j in range(len(train_set[i])):
             if train_labels[i] == 1:
-                if train_set[i][j] not in dic_pos:
-                    dic_pos[train_set[i][j]] = 1
-                else:
-                    dic_pos[train_set[i][j]] += 1
+                dic_pos[train_set[i][j]] = dic_pos.get([train_set[i][j]], 0) + 1
             else:
-                if train_set[i][j] not in dic_neg:
-                    dic_neg[train_set[i][j]] = 1
-                else:
-                    dic_neg[train_set[i][j]] += 1
+                dic_neg[train_set[i][j]] = dic_neg.get([train_set[i][j]], 0) + 1
 
     pos_V = len(dic_pos)
     neg_V = len(dic_neg)
@@ -101,12 +95,12 @@ def bigram(train_set, train_labels, dev_set, unigram_smoothing_parameter=0.035, 
 
     for i in range(len(train_set)):
         if train_labels[i] == 1:
-            positive_count_bi += len(train_set[i])-1
+            positive_count_bi += len(train_set[i]) - 1
         else:
-            negative_count_bi += len(train_set[i])-1
+            negative_count_bi += len(train_set[i]) - 1
 
         for j in range(len(train_set[i]) - 1):
-            st = train_set[i][j] + train_set[i][j+1]
+            st = train_set[i][j] + train_set[i][j + 1]
             if train_labels[i] == 1:
                 if st not in dic_pos_bi:
                     dic_pos_bi[st] = 1
@@ -120,8 +114,10 @@ def bigram(train_set, train_labels, dev_set, unigram_smoothing_parameter=0.035, 
 
     pos_V_bi = len(dic_pos_bi)
     neg_V_bi = len(dic_neg_bi)
-    punk_pos_bi = math.log(bigram_smoothing_parameter / (positive_count_bi + bigram_smoothing_parameter * (pos_V_bi + 1)))
-    punk_neg_bi = math.log(bigram_smoothing_parameter / (negative_count_bi + bigram_smoothing_parameter * (neg_V_bi + 1)))
+    punk_pos_bi = math.log(
+        bigram_smoothing_parameter / (positive_count_bi + bigram_smoothing_parameter * (pos_V_bi + 1)))
+    punk_neg_bi = math.log(
+        bigram_smoothing_parameter / (negative_count_bi + bigram_smoothing_parameter * (neg_V_bi + 1)))
 
     # calculation
     res = []
@@ -133,13 +129,15 @@ def bigram(train_set, train_labels, dev_set, unigram_smoothing_parameter=0.035, 
 
         for word in l:
             if word in dic_pos:
-                t = (dic_pos[word] + unigram_smoothing_parameter) / (positive_count + unigram_smoothing_parameter * (pos_V + 1))
+                t = (dic_pos[word] + unigram_smoothing_parameter) / (
+                            positive_count + unigram_smoothing_parameter * (pos_V + 1))
                 pos_total += math.log(t)
             else:
                 pos_total += punk_pos
 
             if word in dic_neg:
-                t = (dic_neg[word] + unigram_smoothing_parameter) / (negative_count + unigram_smoothing_parameter * (neg_V + 1))
+                t = (dic_neg[word] + unigram_smoothing_parameter) / (
+                            negative_count + unigram_smoothing_parameter * (neg_V + 1))
                 neg_total += math.log(t)
             else:
                 neg_total += punk_neg
@@ -147,13 +145,15 @@ def bigram(train_set, train_labels, dev_set, unigram_smoothing_parameter=0.035, 
         for i in range(len(l) - 1):
             st = l[i] + l[i + 1]
             if st in dic_pos_bi:
-                t = (dic_pos_bi[st] + bigram_smoothing_parameter) / (positive_count_bi + bigram_smoothing_parameter * (pos_V_bi + 1))
+                t = (dic_pos_bi[st] + bigram_smoothing_parameter) / (
+                            positive_count_bi + bigram_smoothing_parameter * (pos_V_bi + 1))
                 pos_total_bi += math.log(t)
             else:
                 pos_total_bi += punk_pos_bi
 
             if st in dic_neg_bi:
-                t = (dic_neg_bi[st] + bigram_smoothing_parameter) / (negative_count_bi + bigram_smoothing_parameter * (neg_V_bi + 1))
+                t = (dic_neg_bi[st] + bigram_smoothing_parameter) / (
+                            negative_count_bi + bigram_smoothing_parameter * (neg_V_bi + 1))
                 neg_total_bi += math.log(t)
             else:
                 neg_total_bi += punk_neg_bi
